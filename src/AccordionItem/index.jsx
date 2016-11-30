7b68cf8ef78f2941d742f0c9f14e7756196fbd9f
Vito Goh <vito.goh@razorfish.com>
2016-11-30 14:20:17 -0500

    Apply fix from https://github.com/daviferreira/react-sanfona/pull/97

--- src/AccordionItem/index.jsx
+++ src/AccordionItem/index.jsx
@@ -3,7 +3,6 @@
 import className from 'classnames';
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
-import uuid from 'uuid';
 
 import AccordionItemBody from '../AccordionItemBody';
 import AccordionItemTitle from '../AccordionItemTitle';
@@ -19,83 +18,58 @@ export default class AccordionItem extends Component {
     };
   }
 
-  componentWillMount() {
-    this.uuid = uuid.v4();
+  componentDidMount() {
+    this.setMaxHeight();
   }
 
   componentDidUpdate(prevProps) {
-    if (prevProps.expanded !== this.props.expanded) {
-      if (this.props.expanded) {
-        this.maybeExpand();
+    const { expanded, disabled, children } = this.props;
+
+    if (prevProps.expanded !== expanded) {
+      if (disabled) return;
+
+      if (expanded) {
+        this.handleExpand();
       } else {
         this.handleCollapse();
       }
+    } else if(prevProps.children !== children) {
+      this.setMaxHeight();
     }
-  }
-
-  startTransition() {
-    this.setState({
-      maxHeight: this.maxHeight,
-      overflow: 'hidden'
-    });
-    clearTimeout(this.timeout);
-  }
-
-  maybeExpand() {
-    const { disabled } = this.props;
-
-    if(disabled) {
-      return;
-    }
-
-    const bodyNode = ReactDOM.findDOMNode(this.refs.body);
-    const images = bodyNode.querySelectorAll('img');
-
-    if (images.length > 0) {
-      this.preloadImages(bodyNode, images);
-      return;
-    }
-
-    this.handleExpand();
   }
 
   handleExpand() {
     const { onExpand, slug } = this.props;
 
-    this.startTransition();
-    this.timeout = setTimeout(() => {
-      this.setState({
-        maxHeight: 'none',
-        overflow: 'visible'
-      });
+    this.setMaxHeight();
 
-      if(onExpand) {
-        slug ? onExpand(slug) : onExpand();
-      }
-
-    }, this.state.duration);
+    if(onExpand) {
+      slug ? onExpand(slug) : onExpand();
+    }
   }
 
   handleCollapse() {
-    const { onClose } = this.props;
+    const { onClose, slug } = this.props;
 
-    this.startTransition();
-    this.timeout = setTimeout(() => {
-      this.setState({
-        maxHeight: 0,
-        overflow: 'hidden'
-      });
+    this.setMaxHeight();
 
-      if(onClose) {
-        onClose();
-      }
-
-    }, 0);
+    if(onClose) {
+      slug ? onClose(slug) : onClose();
+    }
   }
 
-  get maxHeight() {
-    const body = ReactDOM.findDOMNode(this.refs.body);
-    return `${body.scrollHeight}px`;
+  setMaxHeight() {
+    var bodyNode = ReactDOM.findDOMNode(this.refs.body);
+    var images = bodyNode.querySelectorAll('img');
+
+    if (images.length > 0) {
+      return this.preloadImages(bodyNode, images);
+    }
+
+    this.setState({
+      maxHeight: this.props.expanded ? bodyNode.scrollHeight + 'px' : 0,
+      overflow: 'hidden'
+    });
   }
 
   // Wait for images to load before calculating maxHeight
@@ -105,7 +79,10 @@ export default class AccordionItem extends Component {
       imagesLoaded++;
 
       if (imagesLoaded === images.length) {
-        this.handleExpand();
+        this.setState({
+          maxHeight: this.props.expanded ? node.scrollHeight + 'px' : 0,
+          overflow: 'hidden'
+        });
       }
     };
 
@@ -121,13 +98,12 @@ export default class AccordionItem extends Component {
       className: className(
         'react-sanfona-item',
         this.props.className,
-        { 'react-sanfona-item-expanded': (this.props.expanded && !this.props.disabled ) },
+        { 'react-sanfona-item-expanded': (this.props.expanded && !this.props.disabled)},
         this.props.expandedClassName && { [this.props.expandedClassName]: this.props.expanded },
         { 'react-sanfona-item-disabled': this.props.disabled },
         this.props.disabledClassName && { [this.props.disabledClassName]: this.props.disabled },
       ),
       role: 'tabpanel',
-      tabIndex: '0',
       style: this.props.style
     };
 
@@ -140,30 +116,22 @@ export default class AccordionItem extends Component {
     return props;
   }
 
-  handleKeyDown(e) {
-    if (e.keyCode === 13) {
-      if (typeof this.props.onKeyDown === 'function') {
-          this.props.onKeyDown();
-      }
-    }
-  }
-
   render() {
     return (
-      <div {...this.getProps()} ref="item" onKeyDown={this.handleKeyDown.bind(this)}>
+      <div {...this.getProps()} ref="item">
         <AccordionItemTitle
           className={this.props.titleClassName}
           title={this.props.title}
           onClick={this.props.disabled ? null : this.props.onClick}
           titleColor= {this.props.titleColor}
-          uuid={this.uuid} />
+          uuid={`${this.props.title.toLowerCase().replace(/\s/g, '-')}-${this.props.index}`} />
         <AccordionItemBody
           maxHeight={this.state.maxHeight}
           duration={this.state.duration}
           className={this.props.bodyClassName}
           overflow={this.state.overflow}
           ref="body"
-          uuid={this.uuid}>
+          uuid={`${this.props.title.toLowerCase().replace(/\s/g, '-')}-${this.props.index}`}>
           {this.props.children}
         </AccordionItemBody>
       </div>
@@ -177,8 +145,6 @@ AccordionItem.propTypes = {
   className: PropTypes.string,
   expanded: PropTypes.bool,
   onClick: PropTypes.func,
-  onKeyDown: PropTypes.func,
-  onFocus: PropTypes.func,
   title: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.node,
